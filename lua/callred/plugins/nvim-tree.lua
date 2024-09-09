3a05e9c47d07b56aa0a3752bd0aaebcf54a341f4
christian <christianallred@gmail.com>
2024-09-08 23:03:25 -0600

    better

--- lua/callred/plugins/nvim-tree.lua
+++ lua/callred/plugins/nvim-tree.lua
@@ -1,20 +1,20 @@
 return {
-	"nvim-tree/nvim-tree.lua",
-	enabled = true,
-	event = "VeryLazy",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		local nvimtree = require("nvim-tree")
-		-- nvimtree setup
-		nvimtree.setup({
-			filters = {
-				dotfiles = false,
-				custom = {
-					"^.git$",
-				},
-			},
-		})
-		vim.keymap.set("n", "<leader>E", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
-		vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeFindFiles<CR>", { desc = "Find Files explorer" })
-	end,
+  "nvim-tree/nvim-tree.lua",
+  enabled = true,
+  event = "VeryLazy",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    local nvimtree = require("nvim-tree")
+    -- nvimtree setup
+    nvimtree.setup({
+      filters = {
+        dotfiles = false,
+        custom = {
+          "^.git$",
+        },
+      },
+    })
+    vim.keymap.set("n", "<leader>E", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+    vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeFindFile<CR>", { desc = "Find Files explorer" })
+  end,
 }
