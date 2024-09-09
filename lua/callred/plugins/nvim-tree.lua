8c5761da950d81985865e321cd5769917173d027
christian <christianallred@gmail.com>
2024-09-08 23:02:48 -0600

    better finding

--- lua/callred/plugins/nvim-tree.lua
+++ lua/callred/plugins/nvim-tree.lua
@@ -1,19 +1,20 @@
 return {
 	"nvim-tree/nvim-tree.lua",
-    enabled = true,
+	enabled = true,
 	event = "VeryLazy",
-    dependencies = {"nvim-tree/nvim-web-devicons"},
-    config = function()
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
 		local nvimtree = require("nvim-tree")
 		-- nvimtree setup
 		nvimtree.setup({
-            filters = {
-                dotfiles = false,
-                custom = {
-                    "^.git$"
-                }
-            }
+			filters = {
+				dotfiles = false,
+				custom = {
+					"^.git$",
+				},
+			},
 		})
-		vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+		vim.keymap.set("n", "<leader>E", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+		vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeFindFiles<CR>", { desc = "Find Files explorer" })
 	end,
 }
